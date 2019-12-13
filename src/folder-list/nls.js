b54857f57a9a1abd77dd78bf1be596f3ceeee1fb
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-13 13:50:02 +0000

    Option 1. Display of the files and folders stats using array to handle async

--- src/folder-list/nls.js
+++ src/folder-list/nls.js
@@ -1,8 +1,49 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 
-fs.readdir(".", (err, filenames) => {
+// using cwd command
+fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
     throw new Error(err);
   }
   console.log(filenames);
+
+  // Possible but not optimal solution using array, filling it untill everything is ready.
+
+  const allStats = Array(filenames.length).fill(null);
+
+  for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
+
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      allStats[index] = stats;
+      const ready = allStats.every(stats => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach(() => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
+
+//optional readdir call using "."
+// fs.readdir(".", (err, filenames) => {
+//   if (err) {
+//     throw new Error(err);
+//   }
+//   console.log(filenames);
+// });
+
+// How to setup:
+// - in package.json add "bin": { "nls": "./src/folder-list/nls.js" }
+// - in nls.js file add #!/usr/bin/env node
+// - run npm link

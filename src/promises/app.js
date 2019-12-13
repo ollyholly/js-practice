fea5f424452b7c24ed4558674603a3998f4b8235
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-13 23:12:35 +0000

    Rewrite promises exercise

--- src/promises/app.js
+++ src/promises/app.js
@@ -1,14 +1,36 @@
-const willGetYouADog = new Promise((resolve, reject) => {
-  const rand = Math.random();
-  if (rand < 0.5) {
-    resolve();
-  } else {
-    reject();
-  }
+const randomTimeout = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const rand = Math.random();
+    if (rand < 0.5) {
+      reject();
+    } else {
+      resolve();
+    }
+  }, 500);
 });
-willGetYouADog.then(() => {
-  console.log("YAY WE GOT A DOG!!!!");
-});
-willGetYouADog.catch(() => {
-  console.log(":( NO DOG");
-});
+
+randomTimeout
+  .then(() => {
+    console.log("🥳 Resolved!!");
+  })
+  .catch(() => {
+    console.log("🤯 Rejected!!");
+  });
+
+// const willGetYouADog = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const rand = Math.random();
+//     if (rand < 0.5) {
+//       resolve();
+//     } else {
+//       reject();
+//     }
+//   }, 500);
+// });
+// willGetYouADog
+//   .then(() => {
+//     console.log("YAY WE GOT A DOG!!!!");
+//   })
+//   .catch(() => {
+//     console.log(":( NO DOG");
+//   });
